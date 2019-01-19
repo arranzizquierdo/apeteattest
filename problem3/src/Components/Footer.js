99e8513770b2429a8d5deb61fccd1defd7af83ec
arranzizquierdo <laura.arranz88@gmail.com>
2019-01-19 22:03:03 +0100

    Create footer scss

--- problem3/src/Components/Footer.js
+++ problem3/src/Components/Footer.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import TextFooter from './TextFooter';
+import './Footer.scss';
 
 class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
         return (
             <footer className="footer"> 
